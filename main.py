d8227307372f27799b1118e981de86b27960f11e
ManojMohatra <131569518+ManojMohatra@users.noreply.github.com.>
2025-07-30 12:42:01 +0545

    add tabulate library  and allow users to view all expenses and other data in a table

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from tabulate import tabulate
 import csv
 import os 
 
 def main():
     ensure_csv_file()  #Ensures that the header and csv file exists
 
-    print("welcome to expenses tracker!\n")
+    print("Welcome to expenses tracker!\n")
     print("1. Add a new expense")
     print("2. View all expenses")
     print("3. View summary(by category,by date,total)")
@@ -16,6 +17,8 @@ def main():
     choice  = input("What do you want to do ? ").strip()
     if choice  == "1":
         add_new_entry()
+    elif choice == "2":
+        view_all_expenses()
     
 
 
@@ -23,13 +26,15 @@ def add_new_entry():
     category = input("Category(Food,Rent,Tution,Misc):").strip()
     expense = input("Expense(Amount):").strip()
     date = input("Date(Leave it empty to set the date to now):").strip()
-
+    print("")
     if not date:
         date = datetime.now().strftime("%Y-%m-%d")
 
     with open("expenses.csv",mode="a",newline="") as file:
         writer  = csv.writer(file)
         writer.writerow([category,expense,date])
+
+    print(f"Added: {category} | {expense} | {date}")
 
 
 
@@ -40,5 +45,11 @@ def ensure_csv_file():
             writer = csv.writer(file)
             writer.writerow(["category","expense","date"])
 
+def view_all_expenses():
+    with open("expenses.csv","r") as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        data = list(reader)
+        print(tabulate(data, headers=headers,tablefmt='psql'))
 
 main()
