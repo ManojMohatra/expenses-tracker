ab4906a5f128fb38b2a043774f701ded569f2eb4
ManojMohatra <131569518+ManojMohatra@users.noreply.github.com.>
2025-07-30 11:46:03 +0545

    add expense entry with optional date input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def main():
     print("welcome to expenses tracker!\n")
     print("1. Add a new expense")
@@ -5,11 +7,24 @@ def main():
     print("3. View summary(by category,by date,total)")
     print("4. Delete an expense")
     print("5. Export to file (csv/json)")
-    print("6. Quit")
-    print("")
+    print("6. Quit \n")
 
-    choice  = input("What do you want to do ? ")
-    print(f"You entered {choice}")
+    choice  = input("What do you want to do ? ").strip()
+    if choice  == "1":
+        add_new_entry()
+    
+
+
+def add_new_entry():
+    category = input("Category(Food,Rent,Tution,Misc):").strip()
+    expense = input("Expense(Amount):").strip()
+    date = input("Date(Leave it empty to set the date to now):").strip()
+
+    if not date:
+        date = datetime.now().strftime("%Y-%m-%d")
+
+    
+
 
 
 main()
