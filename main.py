95e242ebc4adefac5e99c1023d5f576d1d38ee14
ManojMohatra <131569518+ManojMohatra@users.noreply.github.com.>
2025-07-31 20:48:54 +0545

    Add delete expense feature and improve input validation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tabulate import tabulate
 import csv
 import os 
 from collections import defaultdict
+import sys
 
 def main():
     ensure_csv_file()  #Ensures that the header and csv file exists
@@ -23,12 +24,19 @@ def main():
         view_all_expenses()
     elif choice == "3":
         view_summary()
+    elif choice == "4":
+        delete_expense()
     
 
 
 def add_new_entry():
-    category = input("Category(Food,Rent,Tution,Misc):").strip().lower()
-    expense = input("Expense(Amount):").strip()
+    category = input("Category(Food,Rent,Tution,Misc,...):").strip().lower()
+    try:
+        expense = float(input("Expense(Amount):").strip())
+    except ValueError:
+        print("Invalid expense amount.Please Enter a number")
+        return
+
     date = input("Date(Leave it empty to set the date to now):").strip()
     print("")
     if not date:
@@ -38,7 +46,7 @@ def add_new_entry():
         writer  = csv.writer(file)
         writer.writerow([category,expense,date])
 
-    print(f"Added: {category} | {expense} | {date}")
+    print(f"Added: {category} | {expense} | {date}",end="")
 
 
 
@@ -97,5 +105,46 @@ def calc_by_field(field):
 def print_summary(data,headers):
     print(tabulate(data.items(),headers=headers,tablefmt="psql"))
 
+def delete_expense():
+    print("1.Delete last expense added to the database.")
+    print("2.Manually select the expense to delete from the database.")
+
+    choice = input("\nWhat do you want to do ? " ).strip()
+
+    with open("expenses.csv","r",newline="") as file:
+            reader = list(csv.reader(file))
+                
+    if choice ==  "1":
+        if len(reader) > 1:
+            delete_row(len(reader)-1,reader)
+        else:
+            print("Nothing to remove")
+        
+    elif choice == "2":
+       for index,row in enumerate(reader):
+                print(f"\nRow:{index} {row}")
+       try:
+            index = int(input("\nWhich row do you want do you want to delete? ").strip())
+            if index >= len(reader) or index == 0:
+                print("Invalid row number(0 is a header)")
+       
+            delete_row(index,reader)
+       except ValueError:
+            print("Invalid input.Please enter a valid number.")
             
+       
+             
+
+def delete_row(choice,reader):
+        choice = int(choice)
+        deleted_row = reader[choice]
+
+        with open("expenses.csv","w",newline="") as file:
+            writer = csv.writer(file)
+            for index,row in enumerate(reader):
+                if index != choice:
+                    writer.writerow(row)
+        print(f"\nDeleted: {deleted_row}",end="")
+
+
 main()
